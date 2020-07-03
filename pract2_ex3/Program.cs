6e2a655cb10396542c4c257cac2347a37a70670f
Даниил Стефаненков <daniilstef2391@gmail.com>
2020-07-03 11:07:30 +0300

    Добавьте файлы проекта.

---
+++ pract2_ex3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract2_ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите цену 1 шт. товара: ");
+                double goods = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine();
+                for (int i = 10; i < 101; i += 10)
+                {
+                    Console.WriteLine("Стоимость " + i + " шт: " + goods * i);
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Неверный формат ввода данных");
+            }
+            Console.ReadKey();
+        }
+    }
+}
